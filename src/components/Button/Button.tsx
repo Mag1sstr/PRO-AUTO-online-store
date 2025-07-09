72f1a5fee74e591639698ddb871f28b0b93fe0fb
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-07-09 18:07:04 +0500

    update Header

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,52 @@
+import styles from "./Button.module.scss";
+
+interface IProps {
+  children: React.ReactNode;
+  width: number | string;
+  height: number | string;
+  start?: boolean;
+  end?: boolean;
+  center?: boolean;
+  onClick?: () => void;
+  color?: string;
+  borderColor?: string;
+  fontSize?: string | number;
+}
+
+function Button({
+  children,
+  width,
+  height,
+  onClick,
+  color = "#fff",
+  borderColor = "#C53720",
+  start,
+  center,
+  end,
+  fontSize = 14,
+}: IProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: center
+          ? "center"
+          : start
+          ? "start"
+          : end
+          ? "end"
+          : "start",
+      }}
+    >
+      <button
+        onClick={onClick}
+        style={{ width, height, fontSize, color, borderColor }}
+        className={styles.btn}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,80 @@
+import styles from "./Header.module.scss";
+import logoImg from "../../assets/header/logo.svg";
+import Button from "../Button/Button";
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__top}>
+        <div className="container">
+          <div className={styles.row}>
+            <img src={logoImg} alt="logo" />
+            <nav>
+              <ul className={styles.links}>
+                <li className={styles.link}>КОМПАНИЯ</li>
+                <li className={styles.link}>КАТАЛОГ</li>
+                <li className={styles.link}>УСЛУГИ</li>
+                <li className={styles.link}>ИНФОРМАЦИЯ</li>
+                <li className={styles.link}>КОНТАКТЫ</li>
+              </ul>
+            </nav>
+            <Button width={168} height={40} fontSize={12}>
+              ЗАКАЗАТЬ ЗВОНОК
+            </Button>
+            <div className={styles.icons}>
+              <div className={styles.search}>
+                <svg
+                  width="40"
+                  height="40"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M24 18C24 21.3137 21.3137 24 18 24C14.6863 24 12 21.3137 12 18C12 14.6863 14.6863 12 18 12C21.3137 12 24 14.6863 24 18ZM22.8941 26.7226C21.4474 27.536 19.7779 28 18 28C12.4772 28 8 23.5228 8 18C8 12.4772 12.4772 8 18 8C23.5228 8 28 12.4772 28 18C28 20.2954 27.2266 22.4101 25.9263 24.0979L33.4142 31.5858L30.5858 34.4142L22.8941 26.7226Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+              <div className={styles.cart}>
+                <svg
+                  width="40"
+                  height="40"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M33.3594 11L29.9844 23H14V11H33.3594Z"
+                    stroke="white"
+                    strokeWidth="4"
+                  />
+                  <path d="M14 4L14 24.5" stroke="white" strokeWidth="4" />
+                  <path d="M14 6H4" stroke="white" strokeWidth="4" />
+                  <circle
+                    cx="17"
+                    cy="31"
+                    r="3"
+                    stroke="white"
+                    strokeWidth="4"
+                  />
+                  <circle
+                    cx="28"
+                    cy="31"
+                    r="3"
+                    stroke="white"
+                    strokeWidth="4"
+                  />
+                </svg>
+                <div className={styles.counter}>1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
