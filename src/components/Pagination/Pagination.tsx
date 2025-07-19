fef6d95483c472e985c54be4a583eba3f6674da3
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-07-19 18:01:34 +0500

    create pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,37 @@
+import styles from "./Pagination.module.scss";
+
+interface IProps {
+  currentPage: number;
+  totalPages: number;
+  setCurrentPage: (page: number) => void;
+}
+
+function Pagination({ currentPage, setCurrentPage, totalPages }: IProps) {
+  const handleClickPage = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    totalPages && (
+      <section className={styles.wrapper}>
+        <ul className={styles.row}>
+          {[...Array(totalPages)].map((_, i) => {
+            const page = i + 1;
+            return (
+              <li
+                className={`${styles.page} ${
+                  currentPage === page && styles.active
+                }`}
+                onClick={() => handleClickPage(page)}
+              >
+                {page}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    )
+  );
+}
+
+export default Pagination;
