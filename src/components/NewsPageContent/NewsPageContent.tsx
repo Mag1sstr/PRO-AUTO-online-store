2bdbdac1193abc77d444fd8cdd501fbd0578da2e
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-08-09 05:19:48 +0500

    update NewsPageContent

--- src/components/NewsPageContent/NewsPageContent.tsx
+++ src/components/NewsPageContent/NewsPageContent.tsx
@@ -5,7 +5,7 @@ import styles from "./NewsPageContent.module.scss";
 function NewsPageContent() {
   const { t, lang } = useLang();
   return (
-    <section className={styles.wraper}>
+    <section className={styles.wrapper}>
       <div className="container">
         <div className={styles.center}>
           <h2 className={styles.title}>{t[lang].news.title}</h2>
@@ -16,11 +16,17 @@ function NewsPageContent() {
             <div className={styles.image__wrapper}>
               <img src="" alt="image" />
             </div>
-            <div className={styles.card_content}>
-              <p className={styles.card__date}></p>
-              <h3 className={styles.card__title}></h3>
-              <p className={styles.card__desc}></p>
-              <Button>ЧИТАТЬ ПОДРОБНЕЕ</Button>
+            <div className={styles.card__content}>
+              <p className={styles.card__date}>24 февраля 2020 года</p>
+              <h3 className={styles.card__title}>
+                МЫ ОБНОВИЛИСЬ. ТЕПЕРЬ У НАС НОВЫЙ ЛОГОТИП И ОБНОВЛЕННЫЙ САЙТ.
+              </h3>
+              <p className={styles.card__desc}>
+                Мы рады ононсировать наш обновленный внешний мир. Мы не стоим на
+                месте, постоянно развиваемся и улучшаем качество наших услуг, а
+                также следим и за своим внешним видом.
+              </p>
+              <Button fontSize={12}>ЧИТАТЬ ПОДРОБНЕЕ</Button>
             </div>
           </div>
         </div>
