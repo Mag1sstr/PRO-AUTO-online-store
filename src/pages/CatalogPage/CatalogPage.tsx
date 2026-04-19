6355acaa9198aae1365805b1beabe58fe3bb5201
Daniil K <danilkarachev234@mail.ru>
2026-04-20 02:34:03 +0500

    update CatalogPage

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -2,8 +2,11 @@ import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Products from "../../components/Products/Products";
+import { useMetadata } from "../../hooks/useMetadata";
 
 function CatalogPage() {
+  useMetadata({ title: "Каталог" });
+
   return (
     <>
       <Header />
